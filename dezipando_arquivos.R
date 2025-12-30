728b36567f8908296792c5174e0ca24fca07a6db
Edbbioeco <edsonbbiologia@gmail.com>
2025-12-30 09:49:02 -0300

    Script para descompactar aquivos .zip.
    Atualização: 30.12.2025 09:48

---
+++ dezipando_arquivos.R
@@ -0,0 +1,15 @@
+# Raster ----
+
+## Descompactando ----
+
+unzip("prodes_mata_atlantica_2024.zip")
+
+## Listando arquivos raster (.tif) ----
+
+list.files(pattern = ".tif")
+
+# Geopackage ----
+
+## Descompactando ----
+
+## Lista de arquivos geopackage (.gpkg) ----
