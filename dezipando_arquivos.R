9c2369c0fdde6324f83515764a68c8c82a433e53
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-30 09:43:07 -0300

    tentando

---
+++ dezipando_arquivos.R
@@ -0,0 +1,19 @@
+# Raster ----
+
+## Descompactando ----
+
+unzip("prodes_mata_atlantica_2024.zip")
+
+## Lista de arquivos raster (.tif) ----
+
+list.files(pattern = ".tif")
+
+# Geopackage ----
+
+## Descompactando ----
+
+unzip("prodes_mata_atlantica_nb.gpkg.zip")
+
+## Lista de arquivos geopackage (.gpkg) ----
+
+list.files(pattern = ".gpkg")
